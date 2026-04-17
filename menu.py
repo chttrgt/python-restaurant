1188fd30516cde0696786c83ea74e98ff2cc47e4
Cihat Turgut <cihatturgut@yaani.com>
2026-04-17 16:07:09 +0300

    changelog: - Added Menu & Categories with using dictionary

---
+++ menu.py
@@ -0,0 +1,46 @@
+MENU = {
+    "Çorbalar": {
+        "Mercimek Çorbası": 60,
+        "Domates Çorbası": 55,
+        "Tavuk Suyu Çorba": 75,
+        "Ezogelin Çorbası": 65,
+        "Yayla Çorbası": 65,
+    },
+    "Ana Yemekler": {
+        "Tavuk Şiş": 180,
+        "Köfte": 160,
+        "Mantı": 150,
+        "Karnıyarık": 170,
+        "Sebzeli Makarna": 120,
+    },
+    "Tatlılar": {
+        "Sütlaç": 80,
+        "Kazandibi": 85,
+        "Baklava": 120,
+        "Profiterol": 110,
+        "Cheesecake": 130,
+    },
+    "Salatalar": {
+        "Çoban Salata": 70,
+        "Mevsim Salata": 75,
+        "Ton Balıklı Salata": 120,
+        "Sezar Salata": 125,
+        "Gavurdağı Salata": 95,
+    },
+    "Sıcak İçecekler": {
+        "Türk Kahvesi": 45,
+        "Latte": 70,
+        "Çay": 20,
+        "Sıcak Çikolata": 65,
+        "Americano": 55,
+    },
+    "Soğuk İçecekler": {
+        "Su": 10,
+        "Ayran": 20,
+        "Kola": 35,
+        "Limonata": 40,
+        "Soğuk Kahve": 75,
+    },
+}
+
+CATEGORIES = list(MENU.keys())
